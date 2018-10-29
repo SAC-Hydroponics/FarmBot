7842c75e5852b632450679bcbfc57c625c31fad1
isaacolson <olsonites@gmail.com>
2018-10-29 16:45:14 -0500

    Update movement_test.py

--- movement_test.py
+++ movement_test.py
@@ -4,13 +4,6 @@
 from farmware_tools import device
 from farmware_tools import get_config_value
 
-# Load inputs from Farmware page widget specified in manifest file
-pos_x = get_config_value('Move Rectangle', 'start_x')
-pos_y = get_config_value('Move Rectangle', 'start_y')
-pos_z = get_config_value('Move Rectangle', 'start_z')
-rectLength = get_config_value('Move Rectangle', 'length')
-rectWidth = get_config_value('Move Rectangle', 'width')
-
 #Define functions
 def moveAbs(x, y, z):
     device.log('Moving to ' + str(x) + ', ' + str(y) + ', ' + str(z), 'success', ['toast'])
